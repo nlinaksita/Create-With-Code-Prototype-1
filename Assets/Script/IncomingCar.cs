9ec085792f8f37a65853134aea8f967b681dbfe2
Nicole L <linaksitan@gmail.com>
2021-05-25 12:41:44 -0700

    Initial commit, working on bonus features such as increasing difficulty and multiplayer

---
+++ Assets/Script/IncomingCar.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncomingCar : MonoBehaviour
+{
+    public float vehicleSpeed = 50f;
+    private float zBound = -12f;
+    public GameObject spawnManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnManager = GameObject.Find("SpawnManager");
+        // Change the rotation of the vehicle to face the player
+        transform.Rotate(new Vector3(0, 180, 0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the car in the vertical direction
+        transform.Translate(Vector3.forward * Time.deltaTime * vehicleSpeed);
+
+        // Destroy the vehicle if it is past the designated zBound
+        if (transform.position.z <= zBound)
+        {
+            spawnManager.GetComponent<SpawnManager>().DecreaseEnemyCount();
+            Destroy(gameObject);
+            vehicleSpeed += 10f;
+            
+        }
+    }
+}
---
+++ Assets/Script/SpawnManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    private float xBounds = 5f;
+    private float zBound = 160f;
+    private Vector3 vehicleRotation = new Vector3(0, 180, 0);
+    public GameObject[] vehiclePrefabs = new GameObject[4];
+    private int waveNumber = 1;
+    private int enemyCount = 0;
+    private float vehicleSpeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnVehicleWave(waveNumber);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemyCount <= 0)
+        {
+            IncreaseDifficulty();
+            SpawnVehicleWave(waveNumber);
+        }
+    }
+
+    void SpawnVehicleWave(int waveNumber)
+    {
+        // Spawn a wave of incoming vehicles
+        for (int i = 0; i < waveNumber; i++)
+        {
+            GameObject vehicle = vehiclePrefabs[Random.Range(0, vehiclePrefabs.Length - 1)];
+            vehicle.GetComponent<IncomingCar>().vehicleSpeed = vehicleSpeed;
+            Instantiate(vehicle, GenerateSpawnPosition(), vehicle.transform.rotation);
+            enemyCount++;
+        }
+    }
+
+    // Increase the wave number if all the vehicles in the wave have been destroyed
+    private void IncreaseDifficulty()
+    {
+        waveNumber++;
+        vehicleSpeed += 10f;
+    }
+
+    // Generate a spawn position
+    private Vector3 GenerateSpawnPosition()
+    {
+        return new Vector3(Random.Range(-xBounds, xBounds), 0, zBound);
+    }
+
+    public void DecreaseEnemyCount()
+    {
+        enemyCount--;
+    }
+}
